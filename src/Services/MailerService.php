acb97406e833b202e96e8c68429c6f8563506538
StevyWebDev <stevywebdev@gmail.com>
2025-03-04 02:37:44 +0100

    add phpmailer

--- src/Services/MailerService.php
+++ src/Services/MailerService.php
@@ -2,88 +2,50 @@
 
 namespace App\Services;
 
-use GuzzleHttp\Exception\GuzzleException;
-use League\OAuth2\Client\Provider\Exception\IdentityProviderException;
-use League\OAuth2\Client\Provider\Google;
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
 class MailerService
 {
-    private string $rootName = '';
-
-    public function __construct()
+    public function send(string $email, string $name): void
     {
-        $this->rootName = $_SERVER['DOCUMENT_ROOT'];
-    }
-
-    /**
-     * @throws GuzzleException
-     * @throws IdentityProviderException
-     */
-    public function send()
-    {
-        // Charger les identifiants OAuth depuis le fichier JSON
-        $root = $this->rootName . '/google.json';
-        $googleCredentials = json_decode(file_get_contents($root), true);
-        $clientId = $googleCredentials['web']['client_id'];
-        $clientSecret = $googleCredentials['web']['client_secret'];
-        $refreshToken = '1//043FfIW1V7RgCCgYIARAAGAQSNwF-L9IrXCha7fYSmwAQeqaBJuBcdFae1R2KfLy9gYTrhTb9sbvCkjwrbhzMmoCzfBG-gP9HSA8';
-
-        // Configuration OAuth2
-        $provider = new Google([
-            'clientId' => $clientId,
-            'clientSecret' => $clientSecret,
-            'redirectUri' => 'https://developers.google.com/oauthplayground',
-        ]);
-
-        try {
-            $oauthToken = $provider->getAccessToken('refresh_token', [
-                'refresh_token' => $refreshToken
-            ]);
-
-            $accessToken = $oauthToken->getToken();  // Utilisez le token pour l'authentification SMTP
-        } catch (IdentityProviderException $e) {
-            die('Erreur OAuth2 : ' . $e->getMessage());
-        }
-
+        //Create an instance; passing `true` enables exceptions
         $mail = new PHPMailer(true);
 
         try {
-            // Configuration du serveur SMTP
-            $mail->isSMTP();
-            $mail->AuthType = 'XOAUTH2';
-            $mail->Host = 'smtp.gmail.com';
-            $mail->SMTPAuth = true;
-            $mail->SMTPSecure = 'tls';
-            $mail->Port = 587;
+            //Server settings
+            $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+            $mail->isSMTP();                                            //Send using SMTP
+            $mail->Host       = 'smtp.mailersend.net';                     //Set the SMTP server to send through
+            $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+            $mail->Username   = 'MS_5enX9U@trial-3z0vkloyoe1g7qrx.mlsender.net';                     //SMTP username
+            $mail->Password   = 'mssp.NtWg7mm.0r83ql3jy2zgzw1j.9Phnth8';                               //SMTP password
+            $mail->SMTPSecure = 'tls';            //Enable implicit TLS encryption
+            $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
-            // Authentification OAuth2
-            $mail->setOAuth(new \PHPMailer\PHPMailer\OAuth([
-                'provider' => $provider,
-                'clientId' => $clientId,
-                'clientSecret' => $clientSecret,
-                'refreshToken' => $refreshToken,
-                'accessToken' => $accessToken,  // Assurez-vous d'utiliser l'access token
-                'userName' => 'phpmailer080@gmail.com'
-            ]));
+            //Recipients
+            $mail->setFrom('MS_5enX9U@trial-3z0vkloyoe1g7qrx.mlsender.net', 'storypaad');
+            $mail->addAddress($email, $name);     //Add a recipient
+//            $mail->addAddress('ellen@example.com');               //Name is optional
+//            $mail->addReplyTo('info@example.com', 'Information');
+//            $mail->addCC('cc@example.com');
+//            $mail->addBCC('bcc@example.com');
 
-            // Expéditeur et destinataires
-            $mail->setFrom('phpmailer080@gmail.com', 'Mailer');
-            $mail->addAddress('vergueiro.steven@gmail.com', 'Joe User');
-            $mail->addReplyTo('phpmailer080@gmail.com', 'Information');
+            //Attachments
+//            $mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
+//            $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
 
-            // Contenu du mail
-            $mail->isHTML(true);
-            $mail->Subject = 'Here is the subject';
-            $mail->Body = 'This is the HTML message body <b>in bold!</b>';
+            //Content
+            $mail->isHTML(true);                                  //Set email format to HTML
+            $mail->Subject = 'Validation de votre compte';
+            $mail->Body    = 'Pour valider votre compte, veuillez cliquer sur le lien suivant: <a href="http://localhost:8000/user/email_link">Valider</a>';
             $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
 
             $mail->send();
             echo 'Message has been sent';
         } catch (Exception $e) {
-            die("Message could not be sent. Mailer Error: {$mail->ErrorInfo}");
+            echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
         }
     }
 }
